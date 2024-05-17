89290d5a3eae039f472777d2aabc34fdb7996b0c
wDrag <haxorus5224@gmail.com>
2024-05-17 10:30:02 +0700

    fix: do not show update image when visit other profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -33,6 +33,8 @@ const Profile = () => {
 
   const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 
+  const isOwner = currentUser.username === username;
+
   useEffect(() => {
     const fetchData = async () => {
       const id = await fetchIdOfProfileOwner(username);
@@ -310,9 +312,9 @@ const Profile = () => {
                   setCover(e.target.files[0]);
                 }}
               />
-              <label htmlFor="coverInput">
+              {isOwner && <label htmlFor="coverInput">
                 <FontAwesomeIcon className="updateCoverBtn" icon={faCamera} />
-              </label>
+              </label>}
 
               <img
                 src={
@@ -332,9 +334,9 @@ const Profile = () => {
                   setPFP(e.target.files[0]);
                 }}
               />
-              <label htmlFor="pfpInput">
+              {isOwner && <label htmlFor="pfpInput">
                 <FontAwesomeIcon icon={faCamera} className="updatePFPBtn" />
-              </label>
+              </label>}
 
               {cover !== null || PFP !== null ? (showSaveImg(), null) : null}
             </div>
